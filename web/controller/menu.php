f7f84180eb95b8445ef39e36d577db473b95800d
DVDet <DVDet@byui.edu>
2019-01-14 23:38:05 -0700

    postgresql table selection test

--- web/controller/menu.php
+++ web/controller/menu.php
@@ -2,7 +2,21 @@
     include 'connection.php';
     $query = "SELECT * FROM menu";   
     $result = pg_exec($db_connection, $query);   
-    echo "Number of rows: " . pg_numrows($result);   
-    pg_freeresult($result);   
+    if ($result) {        
+        echo "The query executed successfully.<br>n";        
+        for ($row = 0; $row < pg_numrows($result); $row++) { 
+            $id = pg_result($result, $row, 'id')
+            $title = pg_result($result, $row, 'title') . " ";        
+            $link = pg_result($result, $row, 'link') . " ";        
+            $parent = pg_result($result, $row, 'parent_menu');        
+            echo "menu: $id<br>n";    
+            echo "menu: $title<br>n";    
+            echo "menu: $link<br>n";    
+            echo "menu: $paremt<br>n";        
+        }        
+    } else {        
+        echo "The query failed with the following error:<br>n";        
+        echo pg_errormessage($db_handle);        
+    }    
     pg_close($db_handle); 
 ?>
