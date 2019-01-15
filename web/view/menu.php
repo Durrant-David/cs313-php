50c5e7c04a0c174171d424db6ddbee760a292f5d
DVDet <DVDet@byui.edu>
2019-01-15 00:05:18 -0700

    menu from database

--- web/view/menu.php
+++ web/view/menu.php
@@ -12,7 +12,7 @@
       <ul class="nav navbar-nav">
         <?php 
           include '../controller/menu.php';
-          $menuItems[] = getMenuItems();
+         // $menuItems[] = getMenuItems();
 //          foreach ($menuItems as $item) {
 //            echo '<li><a href="' . $item["link"] .'">' . $item["title"] . '</a></li>';
 //          }
