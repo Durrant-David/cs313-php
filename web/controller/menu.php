4f618f9280d4d9fc7cf994664f23cc4f401d449c
DVDet <DVDet@byui.edu>
2019-01-14 23:39:42 -0700

    postgresql table selection test

--- web/controller/menu.php
+++ web/controller/menu.php
@@ -5,9 +5,9 @@
     if ($result) {        
         echo "The query executed successfully.<br>n";        
         for ($row = 0; $row < pg_numrows($result); $row++) { 
-            $id = pg_result($result, $row, 'id')
-            $title = pg_result($result, $row, 'title') . " ";        
-            $link = pg_result($result, $row, 'link') . " ";        
+            $id = pg_result($result, $row, 'id');
+            $title = pg_result($result, $row, 'title');        
+            $link = pg_result($result, $row, 'link');        
             $parent = pg_result($result, $row, 'parent_menu');        
             echo "menu: $id<br>n";    
             echo "menu: $title<br>n";    
