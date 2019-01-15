5de1a19e98a2f5c7a47f4d38411dd8df423e5bc8
DVDet <DVDet@byui.edu>
2019-01-14 18:37:31 -0700

    added photos to main page

--- web/index.php
+++ web/index.php
@@ -4,130 +4,29 @@
         <title>David Durrant</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
         <?php include 'view/menu.php'; ?>
-        <div class="bg">
-            <div class="bg-spacer"></div>
-            <div class="row">
-                <div class="col-sm-9"></div>
-                <div class="col-sm-1">
-                    <img src="media/images/IMG_20180919_095658.jpg" id= "personalImg" class="img-circle" alt="David Durrant">  
-                </div>
+        <div class="row bgMtn1">
+            <div class="col-sm-9"></div>
+            <div class="col-sm-1">
+                <img src="media/images/profile.jpg" id="personalImg" class="img-circle" alt="David Durrant">  
             </div>
         </div>
-        <div class="row">
+        <div class="row bg-spacer">
             <div class="col-sm-8">
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
+                My Interests include hiking the various mountains native to Utah. Included are some pictures I have taking on some of my hikes.
+            </div>
+            <div class="col-sm-2">
+            </div>
+        </div>
+        <div class="row bgMtn2">
+            <div class="col-sm-8">
+                
             </div>
             <div class="col-sm-2">
             </div>
