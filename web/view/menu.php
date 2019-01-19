b3fe0bb0028727c855c51fe009a2ba3f1d283b05
DVDet <DVDet@byui.edu>
2019-01-19 12:56:41 -0700

    added base url to menu include

--- web/view/menu.php
+++ web/view/menu.php
@@ -20,8 +20,8 @@
                 }
                 return $protocol . "://" . $_SERVER['HTTP_HOST'] . '/controller/menu.php';
           }
-          //$url = url();
-          include 'controller/menu.php';
+          include url();
+          //include 'controller/menu.php';
           $items = getMenuItems();
           $itemCount = count($items);
           //var_dump($dbResults);
