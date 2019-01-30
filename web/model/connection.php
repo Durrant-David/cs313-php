7806f4e0297be83d5455c1809529c6b3d9039412
Durrant-David <43824342+Durrant-David@users.noreply.github.com>
2019-01-30 14:02:05 -0700

    Removed sensitive information

--- web/model/connection.php
+++ web/model/connection.php
@@ -1,3 +1,3 @@
 <?php
-    $db_connection = pg_connect("host=ec2-54-235-77-0.compute-1.amazonaws.com dbname=dfnsbg9852d9sd user=yeviieqbnducom password=cc35265ef770f913fd0ff27a97e613d7d83e75c124786ead7bdac56272a41936");
+    $db_connection = pg_connect("host=/* your host here */ dbname=/* your database name here */ user=/* your user here */ password=/* your password here */");
 ?>
