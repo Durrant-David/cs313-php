466fc70c0fda012e8e1926ef9d14bb66370575d5
DVDet <DVDet@byui.edu>
2019-03-01 13:15:50 -0700

    API

--- web/Views/lookup/json.php
+++ web/Views/lookup/json.php
@@ -7,7 +7,7 @@ if (isset($_SERVER["HTTP_ORIGIN"]) === true) {
 		"https://foo.app.moxio.com",
 		"https://lorem.app.moxio.com"
 	);
-	if (in_array($origin, *, true) === true) {
+	if (in_array($origin, '*', true) === true) {
 		header('Access-Control-Allow-Origin: ' . $origin);
 		header('Access-Control-Allow-Credentials: false');
 		header('Access-Control-Allow-Methods: GET');
