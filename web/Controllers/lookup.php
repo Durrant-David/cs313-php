442c71e8e948bcd008064021d737978c5fe387e2
David Durrant <dvdet@byui.edu>
2019-07-23 08:17:17 -0600

    testing code

--- web/Controllers/lookup.php
+++ web/Controllers/lookup.php
@@ -12,7 +12,7 @@ defined('_CSEXEC') or die;
         public function displayList()
         {
             // get the database items
-            $items = $this->model->getList($this->filters());
+            /*$items = $this->model->getList($this->filters());
             foreach ($items as $item) 
             {
                 ?>
@@ -20,15 +20,15 @@ defined('_CSEXEC') or die;
                   <td class="col-1">
                       <?php echo $this->activeIcon($item['id'], $item['status']); ?>
                       <a href="edit?id=<?php echo $item["id"]; ?>"><i class="far fa-edit listIcon"></i></a></td>
-<!--
-                  <td class="col-1"><?php echo $item['type']; ?></td>
+
+                  <td class="col-1"><?php /*echo $item['type']; ?></td>
                   <td class="col-2"><?php echo $item['level']; ?></td>
                   <td class="col-4"><?php echo $item['catwalk']; ?></td>
-                  <td class="col-4"><?php echo $item['number']; ?></td>
--->
+                  <td class="col-4"><?php echo $item['number']; </td>
+
                 </tr>  
                 <?php
-            }
+            }*/
         }
 
         public function toJSON()
