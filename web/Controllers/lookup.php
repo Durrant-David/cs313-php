fc9f5ce68d0100d9ca9c8f0467444e0ab93e16a9
David Durrant <dvdet@byui.edu>
2019-07-25 14:24:52 -0600

    database debugging

--- web/Controllers/lookup.php
+++ web/Controllers/lookup.php
@@ -13,13 +13,13 @@ defined('_CSEXEC') or die;
         {
             // get the database items
             $items = $this->model->getList($this->filters());
-//            foreach ($items as $item) 
-//            {
+            foreach ($items as $item) 
+            {
                 ?>
                 <tr>
                   <td class="col-1">
                       <?php /*echo $this->activeIcon($item['id'], $item['status']);*/ ?>
-                      <a href="edit?id=<?php /*echo $item["id"];*/ ?>"><i class="far fa-edit listIcon"></i></a></td>
+                      <a href="edit?id=<?php echo $item["id"]; ?>"><i class="far fa-edit listIcon"></i></a></td>
 
                   <td class="col-1"><?php /*echo $item['type']; ?></td>
                   <td class="col-2"><?php echo $item['level']; ?></td>
@@ -28,7 +28,7 @@ defined('_CSEXEC') or die;
 
                 </tr>  
                 <?php
-//            }
+            }
         }
 
         public function toJSON()
