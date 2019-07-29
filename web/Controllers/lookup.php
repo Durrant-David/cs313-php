3aa68c57c6277da16bf962c0e28ee252935945e5
David Durrant <dvdet@byui.edu>
2019-07-29 17:37:01 -0600

    finished fixing lst view of lookup

--- web/Controllers/lookup.php
+++ web/Controllers/lookup.php
@@ -19,7 +19,7 @@ defined('_CSEXEC') or die;
                 <tr>
                   <td class="col-1">
                       <?php echo $this->activeIcon($item['id'], $item['status']); ?>
-                      <a href="edit?id=<?php echo $item["id"]; ?>"><i class="far fa-edit listIcon"></i></a></td>
+<!--                      <a href="edit?id=<?php /*echo $item["id"];*/ ?>"><i class="far fa-edit listIcon"></i></a></td>-->
 
                   <td class="col-1"><?php echo $item['type']; ?></td>
                   <td class="col-2"><?php echo $item['level']; ?></td>
